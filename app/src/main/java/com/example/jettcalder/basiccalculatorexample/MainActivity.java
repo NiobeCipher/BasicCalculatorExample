dd0df56c739eb6458cc084ca7f58f1dbe775da01
NiobeCipher <jettcalder@gmail.com>
2017-08-25 11:02:06 +0700

    Added Reset Button

--- app/src/main/java/com/example/jettcalder/basiccalculatorexample/MainActivity.java
+++ app/src/main/java/com/example/jettcalder/basiccalculatorexample/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.jettcalder.basiccalculatorexample;
 
+import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //    constants
+    private static final int PLUS = 0;
+    private static final int MINUS = 1;
+    private static final int MUL = 2;
+    private static final int DIV = 3;
 //    activity main components
     private TextInputLayout mNum1;
     private TextInputLayout mNum2;
@@ -15,16 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private Button mMinusBtn;
     private Button mDivBtn;
     private Button mMulBtn;
+    private Button mResetBtn;
     private TextView mResult;
 //    variables
     private Float number1;
     private Float number2;
     private Float result;
-//    constants
-    private static final int PLUS = 0;
-    private static final int MINUS = 1;
-    private static final int MUL = 2;
-    private static final int DIV = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         mMinusBtn = (Button) findViewById(R.id.primary_minus_btn);
         mMulBtn = (Button) findViewById(R.id.primary_mul_btn);
         mDivBtn = (Button) findViewById(R.id.primary_div_btn);
+        mResetBtn = (Button) findViewById(R.id.primary_reset_btn);
         mResult = (TextView) findViewById(R.id.primary_result_tv);
 
 //        Actions
@@ -63,6 +65,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mResult.setText(String.format("Result: %s", calculate(DIV)));
+            }
+        });
+        mResetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mNum1.getEditText().setText("");
+                mNum2.getEditText().setText("");
+                mResult.setText("Result: ");
             }
         });
 
